f4a585281728016d658603008a3813320c625d1f
ChriEffe <christianferrazzano93@gmail.com>
2022-02-02 15:19:31 +0100

    dischi stampati, css da sistemare

---
+++ index-php.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <?php include_once __DIR__ . '/partials/header.php' ?>
+    <main class="bg">
+        <div class="container">
+            <div class="row row-cols-5 pt-5 gx-3">
+                    <?php include __DIR__ . '/partials/cards.php' ?>
+                </div>
+        </div>
+    </main>
+</body>
+</html>
---
+++ partials/cards.php
@@ -0,0 +1,13 @@
+<?php
+include __DIR__ .'/../server/db.php';
+?>
+
+
+    <?php foreach ($cards as $card) { ?>
+        <div class="col-bg">        
+            <img class="img-fluid" src="<?= $card['poster'] ?>" alt="">
+            <h2><?= $card['title'] ?></h2>
+            <h3><?= $card['author'] ?></h3>
+            <h4><?= $card['year'] ?></h4>
+        </div>    
+    <?php } ?> 
